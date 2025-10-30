79cd3ac9f36b2b25773a4514a68ea4b7c1b1dd53
Shashank Naik <shashanknaik2004@gmail.com>
2025-10-31 01:31:40 +0530

    Updated Codes for top 50 SQL Problems

---
+++ LeetCode/LeetCode_175.sql
@@ -0,0 +1,14 @@
+# LeetCode 175 -> Combine Two Tables
+
+#Query
+#***-----------------------------------***
+
+
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM  Person AS p
+LEFT JOIN  Address AS a
+ON p.personId = a.personId
+;
+
+
+#***-----------------------------------***
